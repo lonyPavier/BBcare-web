561de5402d019ac6e552cbfab26f92d92b94413b
bbcare <1261349223@qq.com>
2016-11-01 12:19:25 +0800

    添加lib

---
+++ src/bbcare-base.js
@@ -0,0 +1,19 @@
+var bbcare = {};
+bbcare.comm = {};
+bbcare.config = {};
+/** ==============命名空间定义============= */
+//全局命名空间
+//全局配置
+bbcare.config = {};
+//模型列表
+bbcare.model = {};
+//存储器列表
+bbcare.store = {};
+//控制器列表
+bbcare.control = {};
+//View操作列表
+bbcare.view = {};
+//工具方法列表
+bbcare.util = {};
+//通用方法组件
+bbcare.comm = {};
